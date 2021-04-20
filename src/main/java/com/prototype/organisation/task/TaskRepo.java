ad1246fe4eda18313bd2512b03711478d1ce4748
momoji123 <77244643+momoji123@users.noreply.github.com>
2021-04-20 03:39:12 +0200

    add @Transactional on project repo

--- src/main/java/com/prototype/organisation/task/TaskRepo.java
+++ src/main/java/com/prototype/organisation/task/TaskRepo.java
@@ -3,9 +3,13 @@ package com.prototype.organisation.task;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.prototype.organisation.project.Project;
 
+@Transactional
+@Repository
 public interface TaskRepo extends CrudRepository<Task, Integer> {
 	
 	public List<Task> findAllByProject(Project project);
