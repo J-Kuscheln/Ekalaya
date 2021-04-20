43487d178242ff55c5daee10add32c47093a6873
momoji123 <77244643+momoji123@users.noreply.github.com>
2021-04-20 03:48:31 +0200

    transactional to member repo

--- src/main/java/com/prototype/organisation/member/MemberRepo.java
+++ src/main/java/com/prototype/organisation/member/MemberRepo.java
@@ -3,7 +3,11 @@ package com.prototype.organisation.member;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+@Repository
 public interface MemberRepo extends CrudRepository<Member, UUID> {
 	public Member findByFirstName(String firstName);
 	public Member findByLastName(String lastName);
